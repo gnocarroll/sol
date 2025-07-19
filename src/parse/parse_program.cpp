a6272c899554b0be73cca52642b92205d6db5625
gnocarroll <gocarrol@purdue.edu>
2025-07-18 21:45:07 -0500

    begin working on testing for this STAGE and also allow newlines before any statements

--- src/parse/parse_program.cpp
+++ src/parse/parse_program.cpp
@@ -6,6 +6,8 @@ ast::Program parse_program(ast::ASTBuilder& ast_builder) {
     ast::Scope global_scope;
     ast::CompoundStatement statements;
 
+    parse_zero_plus_newlines(ast_builder);
+
     while (true) {
         auto statement = parse_statement(ast_builder, global_scope);
 
@@ -13,6 +15,8 @@ ast::Program parse_program(ast::ASTBuilder& ast_builder) {
 
         statements.push(std::move(*statement));
     }
+
+    parse_zero_plus_newlines(ast_builder);
 
     return ast::Program(
         std::move(global_scope),
